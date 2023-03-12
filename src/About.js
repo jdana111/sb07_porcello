46ab62f6348209597ded5d789bed52ee23a5e4d9
John Dana <john@danasd.com>
2023-03-12 16:57:43 -0600

    Configure basic routes with links

--- src/About.js
+++ src/About.js
@@ -1,9 +1,13 @@
-import './App.css';
+import { Link } from "react-router-dom";
+import "./App.css";
 
 const About = () => (
   <div>
+    <nav>
+      <Link to="/">Home</Link>
+    </nav>
     <h1>About</h1>
   </div>
-)
+);
 
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import "./App.css";
+import { Link } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <div>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
       <h1>Home</h1>
     </div>
   );
